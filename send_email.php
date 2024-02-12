05c61b3dbf787e4a845d4ac20af587cc53119789
saleem2831 <shaiksaleem2831@gmail.com>
2024-02-12 15:02:32 +0530

    first commit

---
+++ send_email.php
@@ -0,0 +1,66 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer/PHPMailer/src/Exception.php';
+require 'PHPMailer/PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/PHPMailer/src/SMTP.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get form data
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    
+    // Recipient email address
+    $to = 'info@anagramdx.com';
+    
+    // Subject
+    $subject = 'Message from ' . $name;
+    
+    // Email content
+    $email_content = "Name: $name\n";
+    $email_content .= "Email: $email\n";
+    $email_content .= "Message:\n$message\n";
+    
+    // SMTP server configuration
+    $smtpHost = 'smtp.gmail.com';
+    $smtpPort = 587; // TLS port
+    $smtpUsername = 'saleem.s@bgtsol.com';
+    $smtpPassword = 'xvqm ngpb njcg xmlh'; // Your Gmail password or App Password
+    
+    // Create PHPMailer object
+    $mail = new PHPMailer(true);
+    
+    try {
+        // SMTP configuration
+        $mail->isSMTP();
+        $mail->Host = $smtpHost;
+        $mail->SMTPAuth = true;
+        $mail->Username = $smtpUsername;
+        $mail->Password = $smtpPassword;
+        $mail->SMTPSecure = 'tls';
+        $mail->Port = $smtpPort;
+        
+        // Sender and recipient
+        $mail->setFrom($smtpUsername, $name);
+        $mail->addAddress($to);
+        
+        // Email content
+        $mail->isHTML(false);
+        $mail->Subject = $subject;
+        $mail->Body = $email_content;
+        
+        // Send email
+        $mail->send();
+        echo '<p>Your message has been sent successfully. Thank you!</p>';
+    } catch (Exception $e) {
+        echo '<p>Oops! Something went wrong. Please try again later.</p>';
+        echo 'Error: ' . $mail->ErrorInfo;
+    }
+} else {
+    // If the form is not submitted, redirect to the form page
+    header("Location: contact.html");
+    exit;
+}
+?>
